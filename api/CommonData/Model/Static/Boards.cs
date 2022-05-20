f49988ec575e94d216e27fc10adbbd979fc82d9a
Nicky Hansen <nicky-g-hansen@hotmail.com>
2022-05-20 13:27:23 +0200

    remove redundant curly brackets.

--- api/CommonData/Model/Static/Boards.cs
+++ api/CommonData/Model/Static/Boards.cs
@@ -13,18 +13,16 @@ public static class Board
         Serialnumber = "ABC-123-123",
         AttachedComponents = new List<Component>()
         {
+            new Component()
             {
-                new Component()
+                Id = 1,
+                Name = "RGB_DIODE_1",
+                Type = ComponentType.RgbDiode,
+                Pins = new List<Pin>()
                 {
-                    Id = 1,
-                    Name = "RGB_DIODE_1",
-                    Type = ComponentType.RgbDiode,
-                    Pins = new List<Pin>()
-                    {
-                        { new Pin() {Id=1, Direction = PinDirection.Input, Descriptor = "r", HwPinNumber = 10 } },
-                        { new Pin() {Id=2, Direction = PinDirection.Input, Descriptor = "g", HwPinNumber = 11 } },
-                        { new Pin() {Id=3, Direction = PinDirection.Input, Descriptor = "b", HwPinNumber = 12 } },
-                    }
+                     new Pin() {Id=1, Direction = PinDirection.Input, Descriptor = "r", HwPinNumber = 10 },
+                     new Pin() {Id=2, Direction = PinDirection.Input, Descriptor = "g", HwPinNumber = 11 },
+                     new Pin() {Id=3, Direction = PinDirection.Input, Descriptor = "b", HwPinNumber = 12 },
                 }
             }
         }
