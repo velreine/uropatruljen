631fdbd94dcdc8a45ed32440fc892a7248d6c31c
Nicky Hansen <nicky-g-hansen@hotmail.com>
2022-06-08 14:32:08 +0200

    Update IActionFactory interface with run-time type Create method.

--- api/CommonData/Logic/Factory/IActionFactory.cs
+++ api/CommonData/Logic/Factory/IActionFactory.cs
@@ -6,6 +6,7 @@ namespace CommonData.Logic.Factory
     public interface IActionFactory
     {
         TAction CreateAction<TAction>(byte[] rawData) where TAction : IAction;
+        IAction CreateAction(byte[] rawData, Type actionType);
         void RegisterActionCreator(Type typeToCreate, Func<byte[], IAction> creatorFunc);
     }
 }
